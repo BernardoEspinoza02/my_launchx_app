d567c617c97b5287ff83ac29e229a259484cd469
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-23 00:39:27 -0500

    Se agrego codigo para pruebas, Se realizo test de aprobacion y error

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,6 +1,8 @@
-describe ("Esto es una suite de pruebas",() =>{
-    test ('Caso de prueba 1', () => {
-        const result = 1 + 2
-        expect(result).toBe (10);
+const MissionComander = require("./../app/missionComander");
+
+describe ("Unit Test for mission Comander Class", () => {
+    test ('1) Create a mission commander objet', () => {
+        const myMissionComander = new MissionComander ("Woopa")
+        expect(myMissionComander.name).toBe("Woopa");
     });
 })
